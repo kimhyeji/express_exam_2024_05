e8b80c4b6277eea640ff86821dc751e4757ad071
kimhyeji <gpwl5589@gmail.com>
2024-05-22 21:17:40 +0900

    mysql db 연동

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 import express from 'express';
+import mysql from "mysql2/promise";
+
+const pool = mysql.createPool({
+    host: "localhost",
+    user: "korad",
+    password: "kor123414",
+    database: "music_list",
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
 
 const app = express();
 const port = 3000;
@@ -14,8 +25,17 @@ const musicList = [
     },
 ];
 
-app.get('/music', (req, res) => {
-  res.json(musicList);
+app.get('/music', async (req, res) => {
+  const [rows] = await pool.query("SELECT * FROM music ORDER BY id DESC");
+
+  res.json(rows);
+});
+
+app.get('/music/:id', async (req, res) => {
+    const { id } = req.params;
+    const [rows] = await pool.query("SELECT * FROM music WHERE id = ?", [id]);
+
+    res.json(rows[0]);
 });
 
 app.listen(port, () => {
